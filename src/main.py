ddddeab8686171168899e6cab3bd72ca2a363395
SaddamAnnais <saddamannaiss@gmail.com>
2023-05-22 19:58:35 +0700

    feat: completing doc

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
-from scipy.ndimage import gaussian_filter
+import cv2
+from scipy.ndimage import gaussian_filter, morphology
 
 
 def mean_threshold(image, k=0.85):
@@ -8,9 +9,9 @@ def mean_threshold(image, k=0.85):
     return np.mean(image_np)*k
 
 
-def adaptive_treshold(block):
+def adaptive_treshold(block, k):
     # Compute treshold
-    threshold = mean_threshold(block)
+    threshold = mean_threshold(block, k)
 
     # Adaptive tresholding for a blok of image
     block = block > threshold
@@ -21,22 +22,22 @@ def adaptive_treshold(block):
     return block
 
 
-def divide_and_conquer(blocks, func):
+def divide_and_conquer(blocks, func, k):
     # divide and conquer approach to apply a function to each block
 
     n_blocks = len(blocks)
     if n_blocks == 1:
-        filtered_blocks = [func(blocks[0])]
+        filtered_blocks = [func(blocks[0], k)]
     else:
         mid = n_blocks // 2
-        left_blocks = divide_and_conquer(blocks[:mid], func)
-        right_blocks = divide_and_conquer(blocks[mid:], func)
+        left_blocks = divide_and_conquer(blocks[:mid], func, k)
+        right_blocks = divide_and_conquer(blocks[mid:], func, k)
         filtered_blocks = left_blocks + right_blocks
 
     return filtered_blocks
 
 
-def enhance_image(image_path, block_size):
+def enhance_image(image_path, block_size, k):
     # load the image
     image = Image.open(image_path).convert('L')
 
@@ -50,7 +51,7 @@ def enhance_image(image_path, block_size):
             blocks.append(block)
 
     # apply a filter to each block in the frequency domain using divide and conquer
-    filtered_blocks = divide_and_conquer(blocks, adaptive_treshold)
+    filtered_blocks = divide_and_conquer(blocks, adaptive_treshold, k)
 
     # merge the blocks back together
     enhanced_image = Image.new('L', (width, height))
@@ -61,15 +62,22 @@ def enhance_image(image_path, block_size):
             enhanced_image.paste(Image.fromarray(enhanced_block), (j, i))
             index += 1
 
+    # Perform morphological closing operation on the enhanced image
+    # enhanced_image = Image.fromarray(morphology.binary_closing(enhanced_image, structure=np.ones((2, 2))))
+
     # return the enhanced image
     return enhanced_image
 
 
 if __name__ == '__main__':
     # example usage
-    adaptive_image = enhance_image('test/test2.jpg', 16)
-    adaptive_image.show()
+    # Proposed Method
+    adaptive_image = enhance_image('test/experiments/test3.jpg', 32, 0.95)
+    adaptive_image.save('test/result/test3-1.jpg')
 
-    image = Image.open('test/test2.jpg').convert('L')
-    global_treshold = adaptive_treshold(image)
-    Image.fromarray(global_treshold).show()
+    # Otsu's Method
+    image1 = cv2.imread('test/experiments/test3.jpg')
+    img = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
+    ret, thresh1 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY + 
+                                            cv2.THRESH_OTSU)     
+    Image.fromarray(thresh1).save('test/result/test3-2.jpg')
